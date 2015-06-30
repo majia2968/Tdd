fd3638a7ed1f8a94e80de4c4916bfaa106398d0b
majia2968 <majia2968@gmail.com>
2015-06-30 16:28:25 -0400

    Initialize project

---
+++ src/main/java/org/springframework/samples/petclinic/web/LoginController.java
@@ -0,0 +1,37 @@
+package org.springframework.samples.petclinic.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.service.LoginService;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LoginController {
+	
+	@Autowired
+	private LoginService loginService;
+
+	@RequestMapping("/welcome")
+	public String login(Model model) {
+		return "login";
+	}
+
+	@RequestMapping("/login")  
+	public ModelAndView onLogin(ModelMap model, HttpServletRequest request, HttpServletResponse res) {
+	    String name = request.getParameter("name");  
+	    String password = request.getParameter("password");  
+	    
+		if (!loginService.isValid()) {
+			model.addAttribute("error", "Invalid user name and password");
+			return new ModelAndView("relogin", model);
+			}
+		String userName = loginService.retrieveName();
+		model.addAttribute("name", "Welcome "+userName+"!");
+		return new ModelAndView("greetings", model);
+	}
+
+}
