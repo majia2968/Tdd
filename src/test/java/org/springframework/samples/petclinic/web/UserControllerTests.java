df358e10fd532d8f9f0a9cc6662a3a2e11b0a27f
majia2968 <majia3080@gmail.com>
2015-07-07 22:20:03 -0400

    Update xml files

--- src/test/java/org/springframework/samples/petclinic/web/UserControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/UserControllerTests.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 
 public class UserControllerTests {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
 
 }
