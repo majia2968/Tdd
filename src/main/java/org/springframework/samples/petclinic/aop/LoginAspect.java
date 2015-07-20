bcdf216a5db597ce464230c382f73f66e7f95625
majia2968 <majia3080@gmail.com>
2015-07-20 00:02:59 -0400

    Fix aop didn't work

--- src/main/java/org/springframework/samples/petclinic/aop/LoginAspect.java
+++ src/main/java/org/springframework/samples/petclinic/aop/LoginAspect.java
@@ -5,14 +5,16 @@ import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.samples.petclinic.web.LoginController;
+import org.springframework.stereotype.Component;
 
+@Component
 @Aspect
 public class LoginAspect {
 
 	protected static Logger logger = LoggerFactory
 			.getLogger(LoginController.class);
-
-	@Before("execution(public ModelAndView welcome())")
+	
+	@Before("execution(* org.springframework.samples.petclinic.web.LoginController.*(..))")
 	public void welcomeAdvice() {
 		logger.debug("hello, there!, welcome to my clinic");
 		System.out.println("print, there!, welcome to my clinic");
