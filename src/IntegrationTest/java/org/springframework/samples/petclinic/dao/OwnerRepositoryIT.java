f55a5ae6300979d1ea4eb31ea600d4be78992f58
majia2968 <majia3080@gmail.com>
2015-11-14 09:36:25 -0500

    Add owner repository test

---
+++ src/IntegrationTest/java/org/springframework/samples/petclinic/dao/OwnerRepositoryIT.java
@@ -0,0 +1,20 @@
+package org.springframework.samples.petclinic.dao;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class OwnerRepositoryIT {
+
+	@Test
+	public void testFindByLastName() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath*:mvc-core-config.xml");
+		OwnerRepository ownerRepository = (OwnerRepository) ctx.getBean("OwnerRepository");
+				
+	}
+
+}
