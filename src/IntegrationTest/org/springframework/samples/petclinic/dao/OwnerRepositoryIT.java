3a360a726016ffac514b473e951d7b93f007f916
majia2968 <majia3080@gmail.com>
2015-11-29 19:28:37 -0500

    UPdate

--- src/IntegrationTest/org/springframework/samples/petclinic/dao/OwnerRepositoryIT.java
+++ src/IntegrationTest/org/springframework/samples/petclinic/dao/OwnerRepositoryIT.java
@@ -1,21 +1,24 @@
 package org.springframework.samples.petclinic.dao;
- 
-import static org.junit.Assert.*;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.when;
 
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.samples.petclinic.dao.OwnerRepository;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:spring/mvc-core-config.xml", "classpath*:spring/business-config.xml"})
+@ActiveProfiles("jpa")
 public class OwnerRepositoryIT {
 
+	@Autowired
+	private OwnerRepository ownerRepositoryImpl;
+	
 	@Test
-	public void testFindByLastName() {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath*:mvc-core-config.xml");
-		OwnerRepository ownerRepository = (OwnerRepository) ctx.getBean("OwnerRepository");
-				
+	public void testFindByLastName1() {
+	
+		ownerRepositoryImpl.findById(0);
+	
 	}
-
 }
